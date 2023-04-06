4f30cc101ce75df123bc38ebc7e641aa3afd90c0
leslieshea <lshea@audigy.com>
2023-04-06 15:15:37 -0700

    Added conditional badge on each card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-export default function Card({ img, rating, reviewCount, location, title, price }) {
+export default function Card({ img, rating, reviewCount, location, title, price, openSpots }) {
+	let badgeText;
+
+	if (openSpots === 0) {
+		badgeText = 'SOLD OUT';
+	} else if (location === 'Online') {
+		badgeText = 'ONLINE';
+	}
+
 	return (
 		<div className="card">
+			{badgeText && <div className="card--badge">{badgeText}</div>}
 			<img
 				src={`../images/${img}`}
 				alt="Katie"
